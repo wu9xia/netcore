159e81eaabeff556389e7f782f2b0f835d9f5f94
wu9xia <wu9xia@sohu.com>
2020-08-20 09:27:41 +0800

    WebAssemblySample

---
+++ BlazorWebAssemblySample1/Data/Store.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using stu = BlazorWebAssemblySample1.Model;
+
+namespace BlazorWebAssemblySample1.Data
+{
+    public class Store
+    {
+        private List<stu.Student> _students;
+
+        public void SetStudents(List<stu.Student> list)
+        {
+            _students = list;
+        }
+
+        public List<stu.Student> GetStudents()
+        {
+            return _students;
+        }
+
+        public stu.Student GetStudentById(int id)
+        {
+            var stu = _students?.FirstOrDefault(s => s.Id == id);
+
+            return stu;
+        }
+    }
+}
