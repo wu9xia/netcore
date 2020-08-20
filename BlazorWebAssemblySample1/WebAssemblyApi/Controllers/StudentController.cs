d3e353ef62cdd7157349705c78353eb91dc1a22e
wu9xia <wu9xia@sohu.com>
2020-08-20 09:37:48 +0800

    WebAssembly first version

---
+++ BlazorWebAssemblySample1/WebAssemblyApi/Controllers/StudentController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Model;
+using WebApplication2.Repository;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private IStudentRepository _studentRepository;
+        public StudentController(IStudentRepository studentRepository)
+        {
+            _studentRepository = studentRepository;
+        }
+
+        [HttpGet]
+        public List<Student> Get()
+        {
+            return _studentRepository.List();
+        }
+
+        [HttpGet("{id}")]
+        public Student Get(int id)
+        {
+            return _studentRepository.Get(id);
+        }
+
+        [HttpPost]
+        public Student Post(Student model)
+        {
+            _studentRepository.Add(model);
+
+            return model;
+        }
+
+        [HttpPut]
+        public Student Put(Student model)
+        {
+            _studentRepository.Update(model);
+
+            return model;
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _studentRepository.Delete(id);
+        }
+    }
+}
---
+++ BlazorWebAssemblySample1/WebAssemblyApi/Repository/Student/IStudentRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication2.Model;
+
+namespace WebApplication2.Repository
+{
+    public interface IStudentRepository
+    {
+        List<Student> List();
+
+        Student Get(int id);
+
+        bool Add(Student student);
+
+        bool Update(Student student);
+
+        bool Delete(int id);
+    }
+}
