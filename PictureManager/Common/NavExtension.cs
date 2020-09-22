a666872dcdbb5e06c03fdedcf6c7b209369820fc
wu9xia <wu9xia@sohu.com>
2020-09-22 14:39:53 +0800

    增加一个Common工程

---
+++ PictureManager/Common/NavExtension.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web;
+
+namespace Common.Utility
+{
+    public static class NavExtension
+    {
+        public static string QueryString(this NavigationManager nav, string paramName)
+        {
+            var uri = nav.ToAbsoluteUri(nav.Uri);
+            string paramValue = HttpUtility.ParseQueryString(uri.Query).Get(paramName);
+            return paramValue ?? "";
+        }
+    }
+}
